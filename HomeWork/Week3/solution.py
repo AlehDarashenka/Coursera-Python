6187a2239d4ccdaa1ac43c2dc9092c1d330dc95f
AlehDarashenka <aleh.darashenka@gmail.com>
2018-01-25 15:14:21 +0300

    hw is added

---
+++ HomeWork/Week3/solution.py
@@ -0,0 +1,15 @@
+class FileReader:
+    def __init__(self, file_path):
+        self.path = file_path
+
+    def read(self):
+        try:
+            with open(self.path, 'r') as f:
+                return f.read()
+        except IOError:
+            return ""
+
+
+if __name__=='__main__':
+    reader = FileReader("D:/my_file.txt")
+    print(reader.read())
